cb081014b47212dd79ab088d1ece84cfecb78470
Bibash Bayalkoti <bayalkotibibash@gmail.com>
2026-06-04 07:39:48 +0545

    run flask app

--- DAY5/app.py
+++ DAY5/app.py
@@ -1,10 +1,14 @@
+# Create the app
 from flask import Flask
 
+# Create Flask app
 app = Flask(__name__)
 
+# Homepage route
 @app.route('/')
 def home():
-    return "Hello Flask."
+    return "Welcome to the Student Performance App!"
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
